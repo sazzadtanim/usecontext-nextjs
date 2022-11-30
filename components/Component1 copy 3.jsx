0820a82e68bfbd22e2955f4b60ad5ecbc540a2f5
sazzadtanim <tanimsazzad@gmail.com>
2022-11-30 21:49:16 +0600

    updated

--- components/Component1 copy 3.jsx
+++ components/Component1 copy 3.jsx
@@ -1,20 +1,20 @@
-import React, {useContext} from 'react';
+import React, {useContext, useRef} from 'react';
 import {usernameContext} from '../pages';
 
 export default function Component4() {
-	const userName = useContext(usernameContext); // 3) useContext to consume the provided
+	const setUsername = useContext(usernameContext);
 	return (
 		<div className="bg-red-700">
 			<h1>This is nested component 4</h1>
 			<p>
-				In Nested component 4 I want the userName without
-				prop drilling
+				In Nested component 4 I want this userName in Main
+				component
 			</p>
 
-			<p>
-				username:
-				<span className="bg-green-400"> {userName}</span>
-			</p>
+			<input
+				placeholder="set user name "
+				onChange={(e) => setUsername(e.target.value)}
+			/>
 		</div>
 	);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,15 @@
 import { createContext, useRef, useState } from "react"
 import Component1 from "../components/Component1"
 
+// todo: reverse dataTransfer from child to parent // useful for notification 
 
-const usernameContext = createContext() //1) create context // 4)export this context for resusibility, but it has to be created in global scope to be exported
+const usernameContext = createContext()
 
 export default function Home() {
   const [userName, setUserName] = useState()
 
   return (
-    //2) wrap under provider
-    //this value provider provides & consumer will consume it
-    // value can be anything (string, function, number ect)
-    // value is like the prop we pass in any component
-    <usernameContext.Provider value={userName}>
+    <usernameContext.Provider value={(cb) => setUserName(cb)}>
       <div className="border border-red-500 bg-red-300">
         <h1>This is the main component</h1>
         <h1>My username is :
